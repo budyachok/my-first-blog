3fd9447f2e69d63c7c797641a79b337350b93a3a
bu <barrrdachok@gmail.com>
2016-02-27 17:06:33 +0200

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,7 @@ class Post(models.Model):
             default=timezone.now)
     published_date = models.DateTimeField(
             blank=True, null=True)
+    file = models.ImageField(upload_to='images', blank=True)
 
     def publish(self):
         self.published_date = timezone.now()
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = [
     # Examples:
@@ -8,4 +9,6 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
      url(r'', include('blog.urls')),
+             url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.MEDIA_ROOT})
 ]
